5665017353bb5aff7c35f6b0a90a3e7df3f2e293
gsadanala <ganesh8374387106@gmail.com>
2024-05-20 13:31:47 -0400

    register users microservice with service registry eureka server for service discovery.

--- keda-service-registry/src/main/java/com/systems/ServiceRegistrationServer.java
+++ keda-service-registry/src/main/java/com/systems/ServiceRegistrationServer.java
@@ -2,14 +2,15 @@ package com.systems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class ServiceRegistrationServer {
     public static void main(String[] args) {
         // Tell Boot to look for registration-server.yml
-    System.setProperty("spring.config.name", "registration-server");
+        System.setProperty("spring.config.additional-location", "classpath:registration-server.yml");
         SpringApplication.run(ServiceRegistrationServer.class, args);
     }
 }
